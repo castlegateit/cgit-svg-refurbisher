f04d5026203fe376e17600c78787db929cee0625
Jmoss <jonathan@castlegateit.co.uk>
2017-07-12 10:45:28 +0100

    Initial build of the Refurbisher.

---
+++ Refurbisher.php
@@ -0,0 +1,6 @@
+<?php
+
+// Load classes and functions
+require_once __DIR__ . '/classes/autoload.php';
+// Initialization
+$condo = new \Cgit\Refurbisher\Refurbisher();
---
+++ classes/Cgit/Refurbisher/Refurbisher.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Cgit\Refurbisher;
+
+class Refurbisher
+{
+
+    /**
+    *  List of SVGs in use.
+    *
+    * @var array
+    */
+
+    public $svg_index;
+
+    /**
+    * Function to include SVG's with deduplication of internal clipping paths.
+    * @param string $path Filepath to SVG.
+    *
+    */
+
+    public function Include_SVG($path) {
+
+        $index = str_replace('.svg', count($this->svg_index), pathinfo($path)['basename']);
+
+        if (count($this->svg_index) && $duplicate = array_search($index, $this->svg_index)) {
+            // We're only good up to 500 duplicate file
+            $index = $duplicate;
+            unset($duplicate);
+        }
+
+        $this->svg_index[] = $index;
+
+        $selectors = array();
+
+        $temp = file_get_contents($path);
+
+        preg_match_all('/<clipPath id="(.*?)">/', $temp, $selectors);
+
+        // Selectors[1] is the array of actual IDs we have captured.
+
+        foreach ($selectors[1] as $key => $value) {
+
+            $temp = preg_replace(
+                 '/<clipPath id="'.$value.'">/',
+                 '<clipPath id="'.$value.'-'.$index.'">',
+                 $temp);
+
+            $temp = preg_replace(
+                                 '/url\(#'.$value.'\)/',
+                                 'url(#'.$value.'-'.$index.')',
+                                 $temp);
+
+        }
+
+        echo $temp;
+
+        // Delete the temp data so we don't duplicate every SVG in memory.
+
+        unset($temp);
+    }
+
+}
